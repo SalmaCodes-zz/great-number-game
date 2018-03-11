230dc7b48dbc80a8e0f493ec81bec051e45fac0b
Salma Suliman <ssuliman91@gmail.com>
2018-03-11 14:30:58 -0700

    Initial commit

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import random
+from time import gmtime, strftime
 app = Flask(__name__)
 app.secret_key = "ThisIsASecret"
 
@@ -7,27 +8,55 @@ app.secret_key = "ThisIsASecret"
 # Index page.
 @app.route('/')
 def index(): 
-    if 'number' not in session.keys():
-        session['state'] = 'new'
-        session['number'] = random.randrange(1, 101)
+    if 'gold' not in session.keys():
+        session['gold'] = 0
+        session['activities'] = []
     return render_template('index.html')
 
 
-@app.route('/guess', methods=['POST'])
-def guess():
-    if int(request.form['number']) == int(session['number']):
-        session['state'] = 'win'
-    elif int(request.form['number']) < int(session['number']):
-        session['state'] = 'low'
-    elif int(request.form['number']) > int(session['number']):
-        session['state'] = 'high'
+@app.route('/process_money', methods=['POST'])
+def process_money():
+    golds = 0
+    time = strftime("%Y/%m/%d %H:%M %p", gmtime())
+    if request.form['building'] == 'farm':
+        golds = random.randrange(10, 21)
+        session['activities'].append({
+            'message': "Earned {} golds from the Farm! ({})".format(golds, time),
+            'color': 'green'
+        })
+    elif request.form['building'] == 'cave':
+        golds = random.randrange(5, 11)
+        session['activities'].append({
+            'message': "Earned {} golds from the Cave! ({})".format(golds, time),
+            'color': 'green'
+        })
+    elif request.form['building'] == 'house':
+        golds = random.randrange(2, 6)
+        session['activities'].append({
+            'message': "Earned {} golds from the House! ({})".format(golds, time),
+            'color': 'green'
+        })
+    elif request.form['building'] == 'casino':
+        golds = random.randrange(-50, 51)
+        if golds >= 0:
+            session['activities'].append({
+                'message': "Earned {} golds from the Casino! ({})".format(golds, time),
+                'color': 'green'
+            })
+        else:
+            session['activities'].append({
+                'message': "Entered a Casino and lost {} golds... Ouch... ({})".format(abs(golds), time),
+                'color': 'red'
+            })
+
+    session['gold'] += golds
 
     return redirect('/')
 
 
 @app.route('/reset', methods=['POST'])
 def reset():
-    session.pop('number')
+    session.pop('gold')
     return redirect('/')
 
 app.run(debug=True)
